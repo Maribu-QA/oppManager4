eb5dd30d7583614abe1563b690c4b36609349ee3
Maribu <maribu2411@gmail.com>
2025-04-15 21:46:40 +0300

    Project done

---
+++ src/test/java/MovieManagerTest.java
@@ -0,0 +1,82 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MovieManagerTest {
+
+    @Test
+    public void shouldAddEmptyArray() {
+        MovieManager manager = new MovieManager();
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldAddArrayLimit() {
+        MovieManager manager = new MovieManager(2);
+
+        manager.addMovie( "Film I" );
+        manager.addMovie( "Film II");
+
+        String[] expected = { "Film I", "Film II"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldAddOneMovie() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+
+        String[] expected = { "Film I" };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldAddMoreMovies() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = { "Film I", "Film II", "Film III" };
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldAddMoreMoviesReverse() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+
+        String[] expected = { "Film III", "Film II", "Film I" };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddMoreMoviesReverseLimit() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film I");
+        manager.addMovie("Film II");
+        manager.addMovie("Film III");
+        manager.addMovie("Film IV");
+        manager.addMovie("Film V");
+
+        String[] expected = { "Film V", "Film IV", "Film III", "Film II", "Film I" };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
